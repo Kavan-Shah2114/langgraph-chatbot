952c8f3980ff795f50157a9da9b770ce4f2548b4
Kavan-Shah2114 <kavanshah2114@gmail.com>
2025-11-21 10:03:01 +0530

    Connected app to Neon PostgreSQL

--- fetch.py
+++ fetch.py
@@ -12,7 +12,8 @@ def get_connection():
         port=st.secrets["DB_PORT"],
         dbname=st.secrets["DB_NAME"],
         user=st.secrets["DB_USER"],
-        password=st.secrets["DB_PASS"]
+        password=st.secrets["DB_PASS"],
+        sslmode="require"
     )
 
 # ---------------- USERS ----------------
